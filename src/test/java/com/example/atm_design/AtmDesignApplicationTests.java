0de9db00b9cf6c269dec21ccf6ed77da5272477b
Ritesh Rijhwani <rijhwaniritesh24@gmail.com>
2025-02-15 16:09:03 +0530

    Test

--- src/test/java/com/example/atm_design/AtmDesignApplicationTests.java
+++ src/test/java/com/example/atm_design/AtmDesignApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.atm_design;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AtmDesignApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
